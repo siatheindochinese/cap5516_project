6c6d37c4978dccbec0a4dcdbb387ef2dfdce3fe4
siatheindochinese <commander_sia@hotmail.com>
2024-04-18 20:14:19 -0400

    added auto eval

---
+++ quantitative.py
@@ -0,0 +1,423 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+import torch.nn as nn
+import torch
+import gc
+import os
+import imageio
+import yaml
+from torch.optim import Adam
+from datetime import datetime
+from PIL import Image
+import requests
+import json
+import nibabel as nib
+import matplotlib
+import math
+import cv2
+import argparse
+
+parser = argparse.ArgumentParser(description ='Person detection, tracking and action recogniton.')
+parser.add_argument("-i", "--idx", default = 484,
+					type = int,
+					help ='index of brats19 brain tumour training dataset')
+parser.add_argument("-r", "--res",
+					type = int, default = 240,
+					help ='width of video')
+args = parser.parse_args()
+
+########################
+# 2D Gaussian Splatter #
+########################
+def generate_2D_gaussian_splatting(kernel_size, sigma_x, sigma_y, rho, coords, colours, image_size=(256, 256, 3), device="cpu"):
+
+    batch_size = colours.shape[0]
+
+    sigma_x = sigma_x.view(batch_size, 1, 1)
+    sigma_y = sigma_y.view(batch_size, 1, 1)
+    rho = rho.view(batch_size, 1, 1)
+
+    covariance = torch.stack(
+        [torch.stack([sigma_x**2, rho*sigma_x*sigma_y], dim=-1),
+        torch.stack([rho*sigma_x*sigma_y, sigma_y**2], dim=-1)],
+        dim=-2
+    )
+
+    # Check for positive semi-definiteness
+    determinant = (sigma_x**2) * (sigma_y**2) - (rho * sigma_x * sigma_y)**2
+    if (determinant <= 0).any():
+        raise ValueError("Covariance matrix must be positive semi-definite")
+
+    inv_covariance = torch.inverse(covariance)
+
+    # Choosing quite a broad range for the distribution [-5,5] to avoid any clipping
+    start = torch.tensor([-5.0], device=device).view(-1, 1)
+    end = torch.tensor([5.0], device=device).view(-1, 1)
+    base_linspace = torch.linspace(0, 1, steps=kernel_size, device=device)
+    ax_batch = start + (end - start) * base_linspace
+
+    # Expanding dims for broadcasting
+    ax_batch_expanded_x = ax_batch.unsqueeze(-1).expand(-1, -1, kernel_size)
+    ax_batch_expanded_y = ax_batch.unsqueeze(1).expand(-1, kernel_size, -1)
+
+    # Creating a batch-wise meshgrid using broadcasting
+    xx, yy = ax_batch_expanded_x, ax_batch_expanded_y
+
+    xy = torch.stack([xx, yy], dim=-1)
+    z = torch.einsum('b...i,b...ij,b...j->b...', xy, -0.5 * inv_covariance, xy)
+    kernel = torch.exp(z) / (2 * torch.tensor(np.pi, device=device) * torch.sqrt(torch.det(covariance)).view(batch_size, 1, 1))
+
+
+    kernel_max_1, _ = kernel.max(dim=-1, keepdim=True)  # Find max along the last dimension
+    kernel_max_2, _ = kernel_max_1.max(dim=-2, keepdim=True)  # Find max along the second-to-last dimension
+    kernel_normalized = kernel / kernel_max_2
+
+    kernel_reshaped = kernel_normalized.repeat(1, 3, 1).view(batch_size * 3, kernel_size, kernel_size)
+    kernel_rgb = kernel_reshaped.unsqueeze(0).reshape(batch_size, 3, kernel_size, kernel_size)
+
+    # Calculating the padding needed to match the image size
+    pad_h = image_size[0] - kernel_size
+    pad_w = image_size[1] - kernel_size
+
+    if pad_h < 0 or pad_w < 0:
+        raise ValueError("Kernel size should be smaller or equal to the image size.")
+
+    # Adding padding to make kernel size equal to the image size
+    padding = (pad_w // 2, pad_w // 2 + pad_w % 2,  # padding left and right
+               pad_h // 2, pad_h // 2 + pad_h % 2)  # padding top and bottom
+
+    kernel_rgb_padded = torch.nn.functional.pad(kernel_rgb, padding, "constant", 0)
+
+    # Extracting shape information
+    b, c, h, w = kernel_rgb_padded.shape
+
+    # Create a batch of 2D affine matrices
+    theta = torch.zeros(b, 2, 3, dtype=torch.float32, device=device)
+    theta[:, 0, 0] = 1.0
+    theta[:, 1, 1] = 1.0
+    theta[:, :, 2] = coords
+
+    # Creating grid and performing grid sampling
+    grid = F.affine_grid(theta, size=(b, c, h, w), align_corners=True)
+    kernel_rgb_padded_translated = F.grid_sample(kernel_rgb_padded, grid, align_corners=True)
+
+    rgb_values_reshaped = colours.unsqueeze(-1).unsqueeze(-1)
+
+    final_image_layers = rgb_values_reshaped * kernel_rgb_padded_translated
+    final_image = final_image_layers.sum(dim=0)
+    final_image = torch.clamp(final_image, 0, 1)
+    final_image = final_image.permute(1,2,0)
+
+    return final_image
+    
+##################################
+# Loss Function and Eval Metrics #
+##################################
+def create_window(window_size, channel):
+    def gaussian(window_size, sigma):
+        gauss = torch.exp(torch.tensor([-(x - window_size//2)**2/float(2*sigma**2) for x in range(window_size)]))
+        return gauss/gauss.sum()
+
+    _1D_window = gaussian(window_size, 1.5).unsqueeze(1)
+    _2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)
+    window = torch.autograd.Variable(_2D_window.expand(channel, 1, window_size, window_size).contiguous())
+
+    return window
+
+def ssim(img1, img2, window_size=11, size_average=True):
+    # Assuming the image is of shape [N, C, H, W]
+    (_, _, channel) = img1.size()
+
+    img1 = img1.unsqueeze(0).permute(0, 3, 1, 2)
+    img2 = img2.unsqueeze(0).permute(0, 3, 1, 2)
+
+
+    # Parameters for SSIM
+    C1 = 0.01**2
+    C2 = 0.03**2
+
+    window = create_window(window_size, channel)
+
+    if img1.is_cuda:
+        window = window.cuda(img1.get_device())
+    window = window.type_as(img1)
+
+    mu1 = F.conv2d(img1, window, padding=window_size//2, groups=channel)
+    mu2 = F.conv2d(img2, window, padding=window_size//2, groups=channel)
+    mu1_sq = mu1.pow(2)
+    mu2_sq = mu2.pow(2)
+    mu1_mu2 = mu1 * mu2
+
+    sigma1_sq = F.conv2d(img1*img1, window, padding=window_size//2, groups=channel) - mu1_sq
+    sigma2_sq = F.conv2d(img2*img2, window, padding=window_size//2, groups=channel) - mu2_sq
+    sigma12 = F.conv2d(img1*img2, window, padding=window_size//2, groups=channel) - mu1_mu2
+
+    SSIM_numerator = ((2*mu1_mu2 + C1)*(2*sigma12 + C2))
+    SSIM_denominator = ((mu1_sq + mu2_sq + C1)*(sigma1_sq + sigma2_sq + C2))
+    SSIM = SSIM_numerator / SSIM_denominator
+
+    return torch.clamp((1 - SSIM) / 2, 0, 1)
+
+def d_ssim_loss(img1, img2, window_size=11, size_average=True):
+    return ssim(img1, img2, window_size, size_average).mean()
+
+# Combined Loss
+def combined_loss(pred, target, lambda_param=0.5):
+    l1loss = nn.L1Loss()
+    return (1 - lambda_param) * l1loss(pred, target) + lambda_param * d_ssim_loss(pred, target)
+
+def calculate_psnr(img1, img2):
+    # img1 and img2 have range [0, 255]
+    img1 = img1.astype(np.float64)
+    img2 = img2.astype(np.float64)
+    mse = np.mean((img1 - img2)**2)
+    if mse == 0:
+        return float('inf')
+    return 20 * math.log10(255.0 / math.sqrt(mse))
+
+def ssim_util(img1, img2):
+    C1 = (0.01 * 255)**2
+    C2 = (0.03 * 255)**2
+
+    img1 = img1.astype(np.float64)
+    img2 = img2.astype(np.float64)
+    kernel = cv2.getGaussianKernel(11, 1.5)
+    window = np.outer(kernel, kernel.transpose())
+
+    mu1 = cv2.filter2D(img1, -1, window)[5:-5, 5:-5]  # valid
+    mu2 = cv2.filter2D(img2, -1, window)[5:-5, 5:-5]
+    mu1_sq = mu1**2
+    mu2_sq = mu2**2
+    mu1_mu2 = mu1 * mu2
+    sigma1_sq = cv2.filter2D(img1**2, -1, window)[5:-5, 5:-5] - mu1_sq
+    sigma2_sq = cv2.filter2D(img2**2, -1, window)[5:-5, 5:-5] - mu2_sq
+    sigma12 = cv2.filter2D(img1 * img2, -1, window)[5:-5, 5:-5] - mu1_mu2
+
+    ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) *
+                                                            (sigma1_sq + sigma2_sq + C2))
+    return ssim_map.mean()
+
+
+def calculate_ssim(img1, img2):
+    '''calculate SSIM
+    the same outputs as MATLAB's
+    img1, img2: [0, 255]
+    '''
+    if not img1.shape == img2.shape:
+        raise ValueError('Input images must have the same dimensions.')
+    if img1.ndim == 2:
+        return ssim(img1, img2)
+    elif img1.ndim == 3:
+        if img1.shape[2] == 3:
+            ssims = []
+            for i in range(3):
+                ssims.append(ssim_util(img1, img2))
+            return np.array(ssims).mean()
+        elif img1.shape[2] == 1:
+            return ssim_util(np.squeeze(img1), np.squeeze(img2))
+    else:
+        raise ValueError('Wrong input image dimensions.')
+ 
+##############
+# Load image #
+##############
+idx = args.idx
+path = "Task01_BrainTumour/imagesTr/BRATS_"+str(idx).zfill(3)+".nii.gz"
+img = nib.load(path)
+img_fdata = img.get_fdata()
+slice = img_fdata[:, :, 78, 1]
+matplotlib.image.imsave('slice.png', slice.T, cmap='gray')
+
+# Read the config.yml file
+with open('config.yml', 'r') as config_file:
+    config = yaml.safe_load(config_file)
+
+# Extract values from the loaded config
+KERNEL_SIZE = int(120 * args.res/240) #config["KERNEL_SIZE"]
+image_size = [args.res, args.res, 3] #tuple(config["image_size"])
+primary_samples = config["primary_samples"]
+backup_samples = config["backup_samples"]
+num_epochs = config["num_epochs"]
+densification_interval = config["densification_interval"]
+learning_rate = config["learning_rate"]
+image_file_name = 'slice.png' #config["image_file_name"]
+display_interval = config["display_interval"]
+grad_threshold = config["gradient_threshold"]
+gauss_threshold = config["gaussian_threshold"]
+display_loss ='true' #config["display_loss"]
+
+##########
+# Config #
+##########
+def give_required_data(input_coords, image_size):
+
+  # normalising pixel coordinates [-1,1]
+  coords = torch.tensor(input_coords / [image_size[0],image_size[1]], device=device).float()
+  center_coords_normalized = torch.tensor([0.5, 0.5], device=device).float()
+  coords = (center_coords_normalized - coords) * 2.0
+
+  # Fetching the colour of the pixels in each coordinates
+  colour_values = [image_array[coord[1], coord[0]] for coord in input_coords]
+  colour_values_np = np.array(colour_values)
+  colour_values_tensor =  torch.tensor(colour_values_np, device=device).float()
+
+  return colour_values_tensor, coords
+  
+###############
+# More Config #
+###############
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+num_samples = primary_samples + backup_samples
+
+PADDING = KERNEL_SIZE // 2
+image_path = image_file_name
+original_image = Image.open(image_path)
+original_image = original_image.resize((240,240))
+#original_image = original_image.resize((image_size[0],image_size[0]))
+original_image = original_image.convert('RGB')
+original_array = np.array(original_image)
+original_array = original_array / 255.0
+width, height, _ = original_array.shape
+
+image_array = original_array
+target_tensor = torch.tensor(image_array, dtype=torch.float32, device=device)
+coords = np.random.randint(0, [width, height], size=(num_samples, 2))
+random_pixel_means = torch.tensor(coords, device=device)
+pixels = [image_array[coord[0], coord[1]] for coord in coords]
+pixels_np = np.array(pixels)
+random_pixels =  torch.tensor(pixels_np, device=device)
+
+colour_values, pixel_coords = give_required_data(coords, image_size)
+
+pixel_coords = torch.atanh(pixel_coords)
+
+sigma_values = torch.rand(num_samples, 2, device=device)
+rho_values = 2 * torch.rand(num_samples, 1, device=device) - 1
+alpha_values = torch.ones(num_samples, 1, device=device)
+W_values = torch.cat([sigma_values, rho_values, alpha_values, colour_values, pixel_coords], dim=1)
+
+##################
+# Moooore Config #
+##################
+starting_size = primary_samples
+left_over_size = backup_samples
+persistent_mask = torch.cat([torch.ones(starting_size, dtype=bool),torch.zeros(left_over_size, dtype=bool)], dim=0)
+current_marker = starting_size
+
+W = nn.Parameter(W_values)
+optimizer = Adam([W], lr=learning_rate)
+loss_history = []
+
+############
+# Training #
+############
+for epoch in range(num_epochs):
+
+    #find indices to remove and update the persistent mask
+    if epoch % (densification_interval + 1) == 0 and epoch > 0:
+        indices_to_remove = (torch.sigmoid(W[:, 3]) < 0.01).nonzero(as_tuple=True)[0]
+
+        if len(indices_to_remove) > 0:
+          print(f"number of pruned points: {len(indices_to_remove)}")
+
+        persistent_mask[indices_to_remove] = False
+
+        # Zero-out parameters and their gradients at every epoch using the persistent mask
+        W.data[~persistent_mask] = 0.0
+
+
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    output = W[persistent_mask]
+
+    batch_size = output.shape[0]
+
+    sigma_x = torch.sigmoid(output[:, 0])
+    sigma_y = torch.sigmoid(output[:, 1])
+    rho = torch.tanh(output[:, 2])
+    alpha = torch.sigmoid(output[:, 3])
+    colours = torch.sigmoid(output[:, 4:7])
+    pixel_coords = torch.tanh(output[:, 7:9])
+
+    colours_with_alpha  = colours * alpha.view(batch_size, 1)
+    g_tensor_batch = generate_2D_gaussian_splatting(KERNEL_SIZE, sigma_x, sigma_y, rho, pixel_coords, colours_with_alpha, image_size, device)
+    loss = combined_loss(g_tensor_batch, target_tensor, lambda_param=0.2)
+
+    optimizer.zero_grad()
+
+    loss.backward()
+
+    # Apply zeroing out of gradients at every epoch
+    if persistent_mask is not None:
+        W.grad.data[~persistent_mask] = 0.0
+
+    if epoch % densification_interval == 0 and epoch > 0:
+
+      # Calculate the norm of gradients
+      gradient_norms = torch.norm(W.grad[persistent_mask][:, 7:9], dim=1, p=2)
+      gaussian_norms = torch.norm(torch.sigmoid(W.data[persistent_mask][:, 0:2]), dim=1, p=2)
+
+      sorted_grads, sorted_grads_indices = torch.sort(gradient_norms, descending=True)
+      sorted_gauss, sorted_gauss_indices = torch.sort(gaussian_norms, descending=True)
+
+      large_gradient_mask = (sorted_grads > grad_threshold)
+      large_gradient_indices = sorted_grads_indices[large_gradient_mask]
+
+      large_gauss_mask = (sorted_gauss > gauss_threshold)
+      large_gauss_indices = sorted_gauss_indices[large_gauss_mask]
+
+      common_indices_mask = torch.isin(large_gradient_indices, large_gauss_indices)
+      common_indices = large_gradient_indices[common_indices_mask]
+      distinct_indices = large_gradient_indices[~common_indices_mask]
+
+      # Split points with large coordinate gradient and large gaussian values and descale their gaussian
+      if len(common_indices) > 0:
+        print(f"number of splitted points: {len(common_indices)}")
+        start_index = current_marker + 1
+        end_index = current_marker + 1 + len(common_indices)
+        persistent_mask[start_index: end_index] = True
+        W.data[start_index:end_index, :] = W.data[common_indices, :]
+        scale_reduction_factor = 1.6
+        W.data[start_index:end_index, 0:2] /= scale_reduction_factor
+        W.data[common_indices, 0:2] /= scale_reduction_factor
+        current_marker = current_marker + len(common_indices)
+
+      # Clone it points with large coordinate gradient and small gaussian values
+      if len(distinct_indices) > 0:
+
+        print(f"number of cloned points: {len(distinct_indices)}")
+        start_index = current_marker + 1
+        end_index = current_marker + 1 + len(distinct_indices)
+        persistent_mask[start_index: end_index] = True
+        W.data[start_index:end_index, :] = W.data[distinct_indices, :]
+        current_marker = current_marker + len(distinct_indices)
+
+    optimizer.step()
+
+    loss_history.append(loss.item())
+
+    if epoch % display_interval == 0:
+        print(f"Epoch {epoch+1}/{num_epochs}, Loss: {loss.item()}, on {len(output)} points")
+     
+#########################
+# Collect PSNR and SSIM #
+#########################
+gt = np.array(Image.open('slice.png').convert('RGB'))
+torch.cuda.empty_cache()
+gc.collect()
+image_size = (240,240,3)
+pred = generate_2D_gaussian_splatting(KERNEL_SIZE, sigma_x * int(240/args.res), sigma_y * int(240/args.res), rho, pixel_coords, colours_with_alpha, image_size, device).cpu().detach().numpy()
+
+psnr_val = calculate_psnr(gt, pred)
+ssim_val = calculate_ssim(gt, pred)
+print('psnr:',psnr_val,'    ','ssim:',ssim_val)
+
+jsonpth = str(args.res)+'.json'
+if jsonpth not in os.listdir(os.getcwd()):
+    json.dump({}, open(jsonpth, 'w'))
+    
+res = json.load(open(jsonpth))
+res[args.idx] = {'psnr': psnr_val, 'ssim': ssim_val}
+json.dump(res, open(jsonpth, 'w'))
---
+++ x2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for i in {1...484}
+do
+   python quantitative.py --idx $i 120
+done
